90ac17bb4dc73c744a7a7abfb1d752480821ff6b
Alina Ulasevich <juliwu2517@gmail.com>
2022-08-04 11:56:01 -0700

    init: human readable number solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    const digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const tens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const decims = ["","", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty", " ninety"];
+
+   let res = '';
+
+   let str = num.toString();
+    if (str.length === 3) {
+
+        res = `${digits[+str[0]]} hundred` ; 
+        if (+str[1] === 1) {
+            res += " " + tens[+str[2]];
+        } else {
+            res = `${res}${decims[+str[1]]} ${ digits[+str[2]]}` ; 
+        }
+    } else if (str.length === 2) {
+        if (+str[0] === 1) {
+            res += tens[+str[1]];
+        } else {
+            res = `${decims[+str[0]]} ${digits[+str[1]]}`;
+        }
+    } else if(str.length === 1) {
+        res = `${+str[0] === 0 ? "zero" : digits[+str[0]]}`;
+    }
+
+    return res.trim();
 }
